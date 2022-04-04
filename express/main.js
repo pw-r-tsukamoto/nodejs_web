26fba242036ad527215d9a5c0834a49a358ebb10
pw-r-tsukamoto <r-tsukamoto@p-world.co.jp>
2022-04-04 16:55:58 +0900

    expressの削除

--- express/main.js
+++
@@ -1,9 +0,0 @@
-const port = 3006;
-const express = require("express");
-const app = express();
-
-app.get("/", (request, response) => {
-    response.send("<h1>Hello Express.js!</h1>");
-}).listen(port, () => {
-    console.log(`The server has started and is listening on port number: ${port}`);
-});
